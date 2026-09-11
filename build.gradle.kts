85d9ce1f4baba874231349c15e38a767e810b538
Jab107-dot <87941972+Jab107-dot@users.noreply.github.com>
2026-09-11 23:04:27 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     // Update the version string here
-    id("com.google.devtools.ksp") version "2.3.6-1.0.24" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
 }
